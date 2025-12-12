d0ea44237c563a7f8e15d643a907b48448fbcb7f
Vrajkumar Shah <vrajshah826@gmail.com>
2025-12-12 21:53:48 +0530

    Create line_plot.py

---
+++ month-1/week-4/hands_on_exercises/line_plot.py
@@ -0,0 +1,12 @@
+# Line Plot (Matplotlib)
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [4, 1, 7, 5]
+
+plt.plot(x, y)
+plt.title("Simple Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
