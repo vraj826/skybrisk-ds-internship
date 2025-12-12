4ebddb197d514cc24fc04daea00540a1c3314969
Vrajkumar Shah <vrajshah826@gmail.com>
2025-12-12 21:40:37 +0530

    Create array.py

---
+++ month-1/week-3/hands_on_exercises/array.py
@@ -0,0 +1,9 @@
+# NumPy: Basic array creation and operations
+
+import numpy as np
+import pandas as pd
+
+arr = np.array([2, 4, 6, 8])
+print("Array:", arr)
+print("Array * 2:", arr * 2)
+print("Array + 5:", arr + 5)
