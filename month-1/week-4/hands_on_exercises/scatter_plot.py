328ef6b71f37176f20671800e4259f1724fa21d0
Vrajkumar Shah <vrajshah826@gmail.com>
2025-12-12 21:54:52 +0530

    Create scatter_plot.py

---
+++ month-1/week-4/hands_on_exercises/scatter_plot.py
@@ -0,0 +1,12 @@
+# Scatter Plot (Matplotlib)
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [10, 20, 30, 25]
+
+plt.scatter(x, y)
+plt.title("Scatter Plot Example")
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
